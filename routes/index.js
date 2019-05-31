73f87d5f0bc11bf13c58372952874b436a67a3b2
Pola Malak <polamalak96@gmail.com>
2019-06-01 01:32:07 +0200

    Search view

--- routes/index.js
+++ routes/index.js
@@ -5,18 +5,22 @@ const bodyParser = require('body-parser');
 router.use(bodyParser.json());
 
 /* GET home page. */
-router.get('/login', function (req, res, next) {
-    res.render('login' , {err:null});
+router.get('/login', function(req, res, next) {
+  res.render('login', { err: null });
 });
 
-router.all('/', function (req, res, next) {
-    res.render('index', {title: 'Express'});
+router.all('/', function(req, res, next) {
+  res.render('index', { title: 'Express' });
 });
 
-router.get('/signup', function (req, res, next) {
-    res.render('signUp' , {err:null});
+router.get('/signup', function(req, res, next) {
+  res.render('signUp', { err: null });
 });
-
-
+router.get('/profile', function(req, res, next) {
+  res.render('profile', { err: null });
+});
+router.get('/search', function(req, res, next) {
+  res.render('search', { err: null });
+});
 
 module.exports = router;
