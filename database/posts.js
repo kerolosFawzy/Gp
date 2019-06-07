aa5cabe3757f093450995061563a17faa1cd4e35
AbanoubTalaatHalim <abanoubtalaat30@gmail.com>
2019-06-07 06:06:06 +0200

    Job Details (Backend) is done

--- database/posts.js
+++ database/posts.js
@@ -1,7 +1,5 @@
-var dbRef = require("./conn");
-
-var postsRef = dbRef.child("posts");
-
+var db = require("./conn");
+var temp;
 module.exports.pushPost = post => {
   postsRef.push(post);
 };
@@ -14,10 +12,14 @@ module.exports.removePost = (postId) => {
   dbRef.ref("posts/" + postId).set(null);
 };
 
-module.exports.getPost = (postId) => {
-    dbRef.ref("posts/" + postId).on('value', function (snap) {
-        return snap.val();
+module.exports.getPost = async (postId) => {
+  console.log("in database ");
+
+  await db.dbRef.child("posts").child(postId).on('value', function (snap) {
+    temp = snap.val();
+    return;
   });
+  return temp;
 };
 
 
