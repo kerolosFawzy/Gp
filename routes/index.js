4e0ef1834d859e1eb0a62189fda8111a7257d7e6
KerolosFawzy <kerofawzy2055@gmail.com>
2019-06-11 12:46:05 +0200

    some changes

--- routes/index.js
+++ routes/index.js
@@ -43,78 +43,83 @@ router.get('/switch', function (req, res, next) {
 
 
 router.get('/hr', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('HR', { logged: bool });
 });
 
 router.get('/admin', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('admin', { logged: bool });
 });
 
 router.get('/profile', function (req, res, next) {
-  let bool = utilies. checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('profile', { logged: bool });
 });
 
 router.get('/addpost', async (req, res, next) => {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   let data = await skillsRef.getSkills();
   if (bool) {
-    if ( utilies.checkHr(bool))
+    if (utilies.checkHr(bool))
       res.render('addpost', { logged: bool, data: data });
   }
   res.render('index', { logged: bool });
 });
 
 router.get('/about', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('about', { logged: bool });
 });
 
 router.get('/contact-us', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('contact-us', { logged: bool });
 });
 
 router.get('/adress_html', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('adress_html', { logged: bool });
 });
 router.get('/search', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('search', { logged: bool });
 });
 
 router.get('/ten', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('topten', { logged: bool });
 });
 
-router.get('/editpost', function (req, res, next) {
-  res.render('editpost');
+router.get('/editpost', (req, res, next) => {
+  let bool = utilies.checkSession(req);
+
+  res.render('editpost', { logged: bool });
+});
+
+router.get('/editprofile', (req, res, next) => {
+  let bool = utilies.checkSession(req);
+  res.render('editprofile', { logged: bool });
 });
 
 
-
-
 router.get('/applied', function (req, res, next) {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
 
   res.render('applicants-applied', { logged: bool });
 });
 
 router.get('/details', async (req, res, next) => {
-  let bool =  utilies.checkSession(req);
+  let bool = utilies.checkSession(req);
   var post = await DbPost.getPost(1);
 
   res.render('job-details', { data: post, logged: bool });
